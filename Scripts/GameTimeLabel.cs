ded327786727a50934e6cf45ab5ff1ea888427af
Aleksandar Gjorgjievski <aleksandargjorgjievski0@gmail.com>
2024-05-28 10:46:00 +0200

    Added a functional timer

--- Scripts/GameTimeLabel.cs
+++ Scripts/GameTimeLabel.cs
@@ -1,20 +1,33 @@
 using Godot;
 using System;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class GameTimeLabel : Label
 {
 	// Called when the node enters the scene tree for the first time.
 
-	RichTextLabel gameTimeTextLabel;
-
-	public override void _Ready()
-	{
-		gameTimeTextLabel = GetNode<RichTextLabel>("GameTimeTextLabel");
-	}
+	// variables for the time
+	float time = 0.0f;
+	int seconds = 0;
+	int minutes = 0;
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		gameTimeTextLabel.Text = "HI";
+		time += (float)delta;
+		seconds = (int)time % 60;
+		minutes = (int)time / 60 % 60;
+		GetNode<RichTextLabel>("Seconds").Text = seconds.ToString("00");
+		GetNode<RichTextLabel>("Minutes").Text = minutes.ToString("00:");
+		StopClock(minutes);
+	}
+
+	public void StopClock (int minutes)
+	{
+		if (minutes >= 10)
+		{
+			SetProcess(false);
+		}
 	}
 }
